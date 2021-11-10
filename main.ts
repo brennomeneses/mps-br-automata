993665e074c9bb10fbb6cd51d121a258bd7cd81c
andersonneumann <anderson.lara@tecpuc.com.br>
2021-11-10 11:13:20 -0300

    Changes

--- main.ts
+++ main.ts
@@ -2,9 +2,10 @@ const { app, BrowserWindow, Menu } = require('electron');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        icon: './assets/components/logo.png'
+        minWidth: 800,
+        minHeight: 600,
+        maxWidth: 800,
+        maxHeight: 600,
     })
 
     win.loadFile('index.html')
